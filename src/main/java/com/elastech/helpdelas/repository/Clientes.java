abe6455e9c739320bb4df941fca89bc5e70ee138
slrocha <slrocha17@gmail.com>
2024-05-03 20:19:56 -0300

    Crud cliente em desenvolvimento

---
+++ src/main/java/com/elastech/helpdelas/repository/Clientes.java
@@ -0,0 +1,7 @@
+package com.elastech.helpdelas.repository;
+
+import com.elastech.helpdelas.model.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public class Clientes extends JpaRepository<Cliente, Integer> {
+}
