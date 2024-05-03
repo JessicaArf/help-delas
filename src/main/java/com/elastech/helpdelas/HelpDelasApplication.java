262be5d65974516428bd8e9a9d087d8413ddfd21
Jessica Arf <jessicalealarf8@gmail.com>
2024-05-02 22:39:22 -0300

    inicio do projeto

---
+++ src/main/java/com/elastech/helpdelas/HelpDelasApplication.java
@@ -0,0 +1,13 @@
+package com.elastech.helpdelas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpDelasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpDelasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elastech/helpdelas/HelpDelasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elastech.helpdelas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpDelasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
