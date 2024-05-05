5a829606b0a50c76c5729661db0f72129505264f
Perkles <gisellekarinna@outlook.com>
2024-05-05 19:55:13 -0300

    feat: Criado o UserDTO, TechService e TechControlle

--- src/main/java/com/elastech/helpdelas/controller/TechController.java
+++ src/main/java/com/elastech/helpdelas/controller/TechController.java
@@ -1,4 +1,32 @@
 package com.elastech.helpdelas.controller;
 
+import com.elastech.helpdelas.dtos.UserDTO;
+import com.elastech.helpdelas.service.TechService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
 public class TechController {
+
+    @Autowired
+    private TechService techService;
+
+    @GetMapping("/salvar-tech")
+    public String showPageRegister(){
+        return "tech/register-tech";
+    }
+
+    @PostMapping("/salvar-tech")
+    public String register(UserDTO userDTO, RedirectAttributes redirectAttributes) throws Exception {
+        try {
+            techService.registerTech(userDTO);
+            return "redirect:/login";
+        } catch (Exception e) {
+            redirectAttributes.addAttribute("error", true);
+            return "redirect:/salvar-tech";
+        }
+    }
 }
---
+++ src/main/java/com/elastech/helpdelas/service/TechService.java
@@ -0,0 +1,49 @@
+package com.elastech.helpdelas.service;
+
+import com.elastech.helpdelas.model.RoleModel;
+import com.elastech.helpdelas.model.UserModel;
+import com.elastech.helpdelas.repositories.RoleRepository;
+import com.elastech.helpdelas.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import com.elastech.helpdelas.dtos.UserDTO;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
+import static com.elastech.helpdelas.dtos.UserDTO.convert;
+
+
+@Service
+public class TechService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+    public UserDTO registerTech(UserDTO userDTO) throws Exception {
+        //Aqui está pegando a role de Tech pelo metodo findByName e salva dentro da variavel roleTech
+        RoleModel roleTech = roleRepository.findByName(RoleModel.Values.TECH.name());
+        //Aqui tesmo um user model optional pois pode ser que não encontre um usuário no BD
+        Optional<UserModel> userExists = userRepository.findByEmail(userDTO.getEmail());
+        //Condição para verificar se o usuário já exite lançando um mensagem
+        if(userExists.isPresent()){
+            throw new Exception("Esse e-mail já existe");
+        }
+        //Pegando a senha do usuário que está sendo cadastrado e passando um metodo de criptografar
+        userDTO.setPassword(passwordEncoder.encode(userDTO.getPassword()));
+        //Passando a role de Tech para esse usuario
+        userDTO.setRole(roleTech);
+        //Utilizando o metodo de converter o userDTO em um userModel
+        UserModel userTech = UserDTO.convert(userDTO);
+        //Salvando
+        userRepository.save(userTech);
+        return new UserDTO(userTech);
+    }
+}
